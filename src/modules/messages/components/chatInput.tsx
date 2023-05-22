6180f7e5595167b5ae12c402c773d3c83d5b85be
FrancisAntai <francisantai25@gmail.com>
2023-05-22 13:39:56 +0100

    worked on  message

--- src/modules/messages/components/chatInput.tsx
+++ src/modules/messages/components/chatInput.tsx
@@ -16,7 +16,7 @@ export const ChatInput = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z" />
                 </svg>
 
-                <ApButton>
+                <ApButton className="cusor-pointer">
                     Send
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5 transform rotate-90">
                         <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
---
+++ src/modules/messages/components/norecents.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Norecents = () => {
+  return (
+    <div>
+        <div className="mt-6 decoration-solid">
+            <p>No messages yet</p>
+        </div>
+     </div>
+  )
+}
+
